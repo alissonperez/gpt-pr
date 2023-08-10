5a1d92264ae48f868458654cfcba8aed2ba8b5ed
Alisson R. Perez <756802+alissonperez@users.noreply.github.com>
2023-08-10 20:18:48 -0300

    feat(lint): setup flake8 lint and fix issues (#10)
    * feat(lint): setup flake8 lint
    
    * fix(lint): fix flake8 issues
    
    * ci(lint): change to run only when pull request

--- gh.py
+++ gh.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import os
 from github import Github
 from InquirerPy import inquirer
@@ -11,13 +10,17 @@ if not GH_TOKEN:
 
 gh = Github(GH_TOKEN)
 
-def create_pr(pr_data):
-    repo = gh.get_repo(f'{pr_data.branch_info.owner}/{pr_data.branch_info.repo}')
 
-    pr_confirmation = inquirer.confirm(message="Create GitHub PR?", default=True).execute()
+def create_pr(pr_data):
+    repo = gh.get_repo(
+        f'{pr_data.branch_info.owner}/{pr_data.branch_info.repo}')
+
+    pr_confirmation = inquirer.confirm(message="Create GitHub PR?",
+                                       default=True).execute()
 
     if pr_confirmation:
-        pr = repo.create_pull(title=pr_data.title, body=pr_data.body, head=pr_data.branch_info.branch, base='main')
+        pr = repo.create_pull(title=pr_data.title, body=pr_data.body,
+                              head=pr_data.branch_info.branch, base='main')
         print("Pull request created successfully: ", pr.html_url)
     else:
         print('cancelling...')
